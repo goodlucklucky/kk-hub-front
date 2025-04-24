3686e838127a39fc0a2d54daaaf5c9f8e4f7a1ee
Jeff Lingad <trendy0413dev@gmail.com>
2025-04-24 13:35:49 -0500

    feat: imnplemented the claim og tab

---
+++ src/app/(pages)/(default)/_components/tasks/task-item.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { CheckIcon } from "@/app/_assets/svg/check";
+import Button from '@/app/_components/shared/button';
+import { CopyIcon } from '@/app/_assets/svg/copy';
+import { ShareIcon } from '@/app/_assets/svg/share';
+
+interface TaskItemProps {
+  title: string;
+  reward: string;
+  taskNumber: number;
+  isCompleted?: boolean;
+  actionTitle?: string;
+  isFriend?: boolean;
+}
+
+const TaskItem: React.FC<TaskItemProps> = ({
+  title,
+  reward,
+  taskNumber,
+  isCompleted = false,
+  actionTitle,
+  isFriend = false
+}) => {
+  return (
+    <div className="rounded-[14px] border border-[#F1DCB8] bg-[#E3BEAA] bg-opacity-50 shadow-[0px_2px_0px_0px_rgba(0,0,0,0.16)] [background:linear-gradient(0deg,rgba(249,227,198,0.50)_0%,rgba(249,227,198,0.50)_100%),#E3BEAA] flex h-full">
+      <div className={`flex flex-col w-6 rounded-[14px_0px_0px_14px] ${!isCompleted ? "bg-[#653F56]" : "bg-[#25BF63]"}`}>
+        <div className={`flex flex-col h-6 rounded-[13px_0px_0px_0px] justify-center items-center text-[#EFF6FF] font-made-tommy font-bold text-xs leading-normal tracking-[0.16px] ${!isCompleted ? "bg-[#82616B]" : "bg-[#609F70]"}`}>
+          {taskNumber}
+        </div>
+        <div className='flex flex-1'>
+        </div>
+      </div>
+      <div className="flex flex-col flex-1 justify-around text-[#5F3F57] font-made-tommy font-bold text-base leading-normal tracking-[0.16px] pt-2 pb-1 px-3 gap-y-1">
+        <div className="flex flex-col">{title}</div>
+        <div className="flex justify-between items-center h-8 rounded-[6px] border-1 border-[#F1DCB8] bg-[#E3BEAA] px-2 pr-1.5">
+          <span className="text-sm font-made-tommy font-bold text-[#745061]">{reward}</span>
+          {
+            isCompleted
+              ?
+              <CheckIcon color="#745061" className="w-4 h-4" />
+              :
+              <Button className='rounded-[2px] bg-gradient-to-b from-[#24BE62] from-[10%] to-[#1AB257] to-[201.67%] py-0.5'>
+                <span className='text-white text-xs font-bold px-2 py-0'>{actionTitle}</span>
+              </Button>
+          }
+        </div>
+        {
+          (isFriend && !isCompleted) && <div className='flex items-center gap-x-1 my-1'>
+            <Button className='rounded-[2px] bg-gradient-to-b from-[#24BE62] from-[10%] to-[#1AB257] to-[201.67%] py-0.5 w-full flex gap-x-1 items-center justify-center'>
+              <ShareIcon className='w-3.5 h-3.5' />
+              <span className='text-white text-xs font-bold py-[1px]'>Share</span>
+            </Button>
+            <Button className='rounded-[2px] bg-gradient-to-b from-[#24BE62] from-[10%] to-[#1AB257] to-[201.67%] py-0.5 w-full flex gap-x-1 items-center justify-center'>
+              <CopyIcon className='w-3.5 h-3.5' />
+              <span className='text-white text-xs font-bold py-[1px]'>Copy Link</span>
+            </Button>
+          </div>
+        }
+      </div>
+    </div>
+  );
+};
+
+export default TaskItem; 
--- src/app/(pages)/(default)/tasks/page.tsx
+++ src/app/(pages)/(default)/tasks/page.tsx
@@ -26,7 +26,12 @@ import cosmetic from '@assets/images/cosmetics.png';
 
 import SkinDialog from "../_components/dialogs/skin-dialog";
 import NavigationButton from "../_components/profile/navigateBtn";
-
+import { XIcon } from "@/app/_assets/svg/x";
+import { TaskIcon } from "@/app/_assets/svg/task";
+import { CheckIcon } from "@/app/_assets/svg/check";
+import TaskItem from "../_components/tasks/task-item";
+import Button from "@/app/_components/shared/button";
+import { ClaimIcon } from "@/app/_assets/svg/claim";
 // Types
 type StoreCategory = 'featured' | 'items' | 'kokitos' | 'social';
 type TasksCategory = 'Claim OG' | 'Koko Tasks' | 'Partner';
@@ -46,7 +51,7 @@ const TasksCategoryButtons = memo(({ activeCategory, onCategoryChange }: { activ
         label={category}
         isActive={activeCategory === category}
         onClick={() => onCategoryChange(category)}
-        className="rounded-[9px] !text-sm !font-normal font-bumper-sticker border-[#9C7B8F]"
+        className="rounded-[9px] !text-sm !font-normal font-bumper-sticker border-[#9C7B8F] bg-[#653F56]"
       />
     ))}
   </div>
@@ -56,19 +61,16 @@ TasksCategoryButtons.displayName = 'TasksCategoryButtons';
 
 export default function TasksPage() {
   const [activeComponent, setActiveComponent] = useState<StoreCategory>("featured");
-  const [activeProductCategory, setActiveProductCategory] = useState<TasksCategory>("Claim OG");
+  const [activeTaskCategory, setActiveTaskCategory] = useState<TasksCategory>("Claim OG");
   const [isSkinDialogOpen, setIsSkinDialogOpen] = useState(false);
+  const [isConnectTwitter, setIsConnectTwitter] = useState(true);
 
   const handleSkinDialogToggle = useCallback(() => {
     setIsSkinDialogOpen(prev => !prev);
   }, []);
 
-  const handleCategoryChange = useCallback((category: StoreCategory) => {
-    setActiveComponent(category);
-  }, []);
-
-  const handleProductCategoryChange = useCallback((category: TasksCategory) => {
-    setActiveProductCategory(category);
+  const handleTaskCategoryChange = useCallback((category: TasksCategory) => {
+    setActiveTaskCategory(category);
   }, []);
 
   return (
@@ -93,39 +95,78 @@ export default function TasksPage() {
           quality={75}
           sizes="100vw"
         />
-        <div className="bg-[url(/images/board_2.png)] flex flex-col gap-3 bg-cover bg-center fixed top-32 bottom-25 w-[95%] mx-auto z-50 border-2 border-[#FAC485] rounded-3xl p-2 right-0 left-0">
-          <div className="bg-[#F5D6B1] rounded-2xl p-3 py-2.5 shadow-md border-2 border-[#A96415] flex flex-col flex-1 overflow-y-auto gap-2">
-            <div className="rounded-[14px] border border-[#F7D8B7] bg-[#DDC2A7] shadow-[inset_0px_2px_0px_0px_rgba(95,63,87,0.20)] [background:linear-gradient(180deg,rgba(95,63,87,0.20)_-577.52%,rgba(95,63,87,0.00)_248.61%),#DDC2A7] p-[5px]">
-              <TasksCategoryButtons
-                activeCategory={activeProductCategory}
-                onCategoryChange={handleProductCategoryChange}
-              />
-            </div>
-            <div className="w-full flex-1 overflow-y-auto rounded-[7px] border-2 border-[#CDAA98] bg-[#E3BEAA] shadow-[inset_0px_4px_0px_0px_rgba(0,0,0,0.20)] p-2 pt-3">
-              <div className="w-full flex flex-col gap-2 bg-[#EED1B8] rounded-[22px] p-3 px-1 overflow-y-auto">
-                <div className="flex justify-start items-center">
-                  <span className="text-[#5F3F57] text-[22px] font-bumper-sticker font-normal px-1">COSMETICS</span>
-                  <DiscountBadge discount={-20} />
-                </div>
-                <div className="justify-between items-center flex-wrap grid grid-cols-3 gap-1">
-                  {Array.from({ length: 9 }).map((_, index) => (
-                    <MemoizedCosmeticCard
-                      key={index}
-                      image={cosmetic}
-                      title="SLUG"
-                      subtitle="SKIN"
-                      price={2.99}
-                      showInfo={index === 0}
-                      isHot={index === 0}
-                      onClick={handleSkinDialogToggle}
-                    />
-                  ))}
+        <div className="bg-[url(/images/board_2.png)] flex flex-col gap-3 bg-cover bg-center fixed top-32 w-[95%] mx-auto z-50 border-2 border-[#FAC485] rounded-3xl p-2 right-0 left-0">
+          <div className="bg-[#F5D6B1] rounded-2xl p-[7px] py-2.5 shadow-md border-2 border-[#A96415] flex flex-col overflow-y-auto gap-2">
+            <div className="flex flex-col flex-1 gap-2 px-2">
+              <div className="rounded-[14px] border border-[#F7D8B7] bg-[#DDC2A7] shadow-[inset_0px_2px_0px_0px_rgba(95,63,87,0.20)] [background:linear-gradient(180deg,rgba(95,63,87,0.20)_-577.52%,rgba(95,63,87,0.00)_248.61%),#DDC2A7] p-[5px]">
+                <TasksCategoryButtons
+                  activeCategory={activeTaskCategory}
+                  onCategoryChange={handleTaskCategoryChange}
+                />
+              </div>
+              {
+                isConnectTwitter
+                  ?
+                  <div className="rounded-[9px] bg-[#EED1B8] [background:linear-gradient(0deg,#D1B69F_0%,#D1B69F_100%),#EED1B8] p-[5px] flex justify-start items-center gap-x-2 px-3">
+                    <XIcon className="w-3.5 h-3.5 mt-[1px]" />
+                    <div className="flex items-center flex-1 gap-x-1">
+                      <span className="h-2 w-2 rounded-full bg-[#126529] gap-x-1"></span>
+                      <span className="text-[#5F3F57] text-shadow-[0px_1px_0px_rgba(0,0,0,0.20)] font-made-tommy text-base font-bold leading-normal tracking-[0.16px]">Connect Twitter</span>
+                    </div>
+                  </div>
+                  :
+                  <div className="rounded-[9px] border border-[#D1AB8D] bg-[#EED1B8] bg-opacity-50 shadow-[0px_1px_0px_0px_rgba(0,0,0,0.20)] flex justify-center items-center gap-x-1 p-[3px]">
+                    <XIcon className="w-3.5 h-3.5 mt-[1px]" />
+                    <span className="text-[#5F3F57] text-shadow-[0px_1px_0px_rgba(0,0,0,0.20)] font-made-tommy text-base font-bold leading-normal tracking-[0.16px]">
+                      Connect Twitter
+                    </span>
+                  </div>
+              }
+              <div className="bg-[#D49FC4] rounded-[10px] p-3 px-4 flex items-center gap-x-2">
+                <TaskIcon />
+                <div className="flex flex-col">
+                  <span className="text-[#5F3F57] text-shadow-[0px_1px_0px_rgba(0,0,0,0.20)] font-made-tommy font-bold leading-normal tracking-[0.16px] text-xs mt-1">
+                    Claim your free Kokomo OG NFT
+                  </span>
+                  <span className="text-[#5F3F57] text-shadow-[0px_1px_0px_rgba(0,0,0,0.20)] font-bumper-sticker font-normal leading-normal tracking-[0.16px] text-lg">
+                    Your free Kokomo OG NFT
+                  </span>
                 </div>
               </div>
             </div>
+            <div className="rounded-[22px] border border-[#FFE0BF] bg-[#DDC2A7] shadow-[inset_0px_2px_1px_0px_rgba(95,63,87,0.20)] [background:linear-gradient(180deg,rgba(95,63,87,0.20)_11.66%,rgba(95,63,87,0.00)_248.61%),#DDC2A7] px-2 py-[9px] flex flex-col gap-y-2">
+              <TaskItem
+                title="Follow us on X"
+                reward="T2 Lootbox"
+                taskNumber={1}
+                isCompleted={true}
+                actionTitle="Verify"
+              />
+              <TaskItem
+                title="Join our Telegram community"
+                reward="T2 Lootbox"
+                taskNumber={2}
+                isCompleted={false}
+                actionTitle="Invite"
+              />
+              <TaskItem
+                title="Invite a Friend"
+                reward="T2 Lootbox"
+                taskNumber={3}
+                isCompleted={false}
+                actionTitle="Invite"
+                isFriend={true}
+              />
+            </div>
+            <div className="flex px-2">
+              <Button className='rounded-[10px] bg-gradient-to-b from-[#24BE62] from-[10%] to-[#1AB257] to-[201.67%] py-0.5 w-full flex gap-x-1 items-center justify-center'>
+                <ClaimIcon className="w-4.5 h-4.5" />
+                <span className='text-white text-lg font-bold py-0.5 font-made-tommy'>Claim</span>
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
+      </div >
       <SkinDialog
         isOpen={isSkinDialogOpen}
         onClose={handleSkinDialogToggle}
