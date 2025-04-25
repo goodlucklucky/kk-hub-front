6b2cc50d06ca70ebf7a5e5841a81decdf04dfea1
Jeff Lingad <trendy0413dev@gmail.com>
2025-04-25 06:36:51 -0500

    feat: implemented the spin and more-spin dialog on spinner page

--- src/app/_assets/svg/copy.tsx
+++ src/app/_assets/svg/copy.tsx
@@ -139,15 +139,40 @@ export function CopyIcon(props: React.ComponentProps<"svg">) {
 export function TapCopyIcon(props: React.ComponentProps<"svg">) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="none" {...props}>
-    <g clipPath="url(#clip0_1137_12881)">
-      <path d="M0 0H6.875V2.75H2.75V6.875H0V0Z" fill="#EED1B8"/>
-      <path d="M11 4.125H4.125V11H11V4.125Z" fill="#EED1B8"/>
-    </g>
-    <defs>
-      <clipPath id="clip0_1137_12881">
-        <rect width="11" height="11" fill="white"/>
-      </clipPath>
-    </defs>
-  </svg>
+      <g clipPath="url(#clip0_1137_12881)">
+        <path d="M0 0H6.875V2.75H2.75V6.875H0V0Z" fill="#EED1B8" />
+        <path d="M11 4.125H4.125V11H11V4.125Z" fill="#EED1B8" />
+      </g>
+      <defs>
+        <clipPath id="clip0_1137_12881">
+          <rect width="11" height="11" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export function InviteCopyIcon(props: React.ComponentProps<"svg">) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="19" height="20" viewBox="0 0 19 20" fill="none" {...props}>
+      <mask id="path-1-outside-1_799_59798" maskUnits="userSpaceOnUse" x="0" y="4" width="15" height="16" fill="black">
+        <rect fill="white" y="4" width="15" height="16" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M9 6H4C2.89543 6 2 6.89543 2 8V14C2 16.2091 3.79086 18 6 18H11C12.1046 18 13 17.1046 13 16V10L9 6Z" />
+      </mask>
+      <path fillRule="evenodd" clipRule="evenodd" d="M9 6H4C2.89543 6 2 6.89543 2 8V14C2 16.2091 3.79086 18 6 18H11C12.1046 18 13 17.1046 13 16V10L9 6Z" fill="white" />
+      <path d="M9 6L10.4142 4.58579C10.0391 4.21071 9.53043 4 9 4V6ZM13 10H15C15 9.46957 14.7893 8.96086 14.4142 8.58579L13 10ZM4 8H9V4H4V8ZM4 8V8V4C1.79086 4 0 5.79086 0 8H4ZM4 14V8H0V14H4ZM6 16C4.89543 16 4 15.1046 4 14H0C0 17.3137 2.68629 20 6 20V16ZM11 16H6V20H11V16ZM11 16V20C13.2091 20 15 18.2091 15 16H11ZM11 10V16H15V10H11ZM14.4142 8.58579L10.4142 4.58579L7.58579 7.41421L11.5858 11.4142L14.4142 8.58579Z" fill="#24BE62" mask="url(#path-1-outside-1_799_59798)" />
+      <mask id="path-3-outside-2_799_59798" maskUnits="userSpaceOnUse" x="4" y="0" width="15" height="16" fill="black">
+        <rect fill="white" x="4" width="15" height="16" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M13 2H8C6.89543 2 6 2.89543 6 4V14H15C16.1046 14 17 13.1046 17 12V6L13 2Z" />
+      </mask>
+      <path fillRule="evenodd" clipRule="evenodd" d="M13 2H8C6.89543 2 6 2.89543 6 4V14H15C16.1046 14 17 13.1046 17 12V6L13 2Z" fill="white" />
+      <path d="M13 2L14.4142 0.585786C14.0391 0.210714 13.5304 0 13 0V2ZM6 14H4C4 15.1046 4.89543 16 6 16V14ZM17 6H19C19 5.46957 18.7893 4.96086 18.4142 4.58579L17 6ZM8 4H13V0H8V4ZM8 4V4V0C5.79086 0 4 1.79086 4 4H8ZM8 14V4H4V14H8ZM15 12H6V16H15V12ZM15 12V16C17.2091 16 19 14.2091 19 12H15ZM15 6V12H19V6H15ZM18.4142 4.58579L14.4142 0.585786L11.5858 3.41421L15.5858 7.41421L18.4142 4.58579Z" fill="#24BE62" mask="url(#path-3-outside-2_799_59798)" />
+      <mask id="path-5-outside-3_799_59798" maskUnits="userSpaceOnUse" x="11" y="0" width="8" height="8" fill="black">
+        <rect fill="white" x="11" width="8" height="8" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M17 6L13 2V4C13 5.10457 13.8954 6 15 6H17Z" />
+      </mask>
+      <path fillRule="evenodd" clipRule="evenodd" d="M17 6L13 2V4C13 5.10457 13.8954 6 15 6H17Z" fill="white" />
+      <path d="M17 6V7.4C17.5662 7.4 18.0767 7.0589 18.2934 6.53576C18.5101 6.01261 18.3903 5.41045 17.9899 5.01005L17 6ZM13 2L13.9899 1.01005C13.5896 0.609653 12.9874 0.489875 12.4642 0.706569C11.9411 0.923262 11.6 1.43375 11.6 2H13ZM17.9899 5.01005L13.9899 1.01005L12.0101 2.98995L16.0101 6.98995L17.9899 5.01005ZM14.4 4V2H11.6V4H14.4ZM15 4.6C14.6686 4.6 14.4 4.33137 14.4 4H11.6C11.6 5.87777 13.1222 7.4 15 7.4V4.6ZM17 4.6H15V7.4H17V4.6Z" fill="#24BE62" mask="url(#path-5-outside-3_799_59798)" />
+    </svg>
   );
 }
