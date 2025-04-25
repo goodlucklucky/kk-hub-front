c07b7017c8445412605e60bef8bddae801fbaa35
Jeff Lingad <trendy0413dev@gmail.com>
2025-04-25 11:15:21 -0500

    feat: implemented the card rotation

--- src/app/(pages)/(default)/_components/dialogs/more-dialog.tsx
+++ src/app/(pages)/(default)/_components/dialogs/more-dialog.tsx
@@ -33,7 +33,7 @@ const MoreSpinsDialog = ({ isOpen, onClose }: MoreSpinsDialogProps) => {
         <DialogOverlay />
         <div className="fixed h-full left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg">
           <>
-            <div className="absolute top-0 left-0 w-full h-full bg-black/75 backdrop-blur-[2.5px]" />
+            <div className="absolute top-0 left-0 w-full h-full backdrop-blur-[2.5px]" />
             <div className="bg-[url(/images/board_2.png)] flex flex-col gap-2 bg-cover bg-center fixed w-[95%] top-1/2 -translate-y-1/2 z-50 border-2 border-[#FAC485] rounded-3xl mx-auto p-2 right-0 left-0">
               <div className="w-full h-16 flex justify-center items-center absolute -top-10">
                 <Image
@@ -52,18 +52,18 @@ const MoreSpinsDialog = ({ isOpen, onClose }: MoreSpinsDialogProps) => {
               </div>
               <div className="bg-[#F5D6B1] rounded-2xl p-3 py-6 shadow-md border-2 border-[#A96415] flex-1 flex flex-col overflow-y-auto gap-2">
                 <div className="flex flex-col gap-y-1">
-                <div className="flex justify-center items-center gap-1">
-                  <span className="text-[#8F6E75] text-center font-made-tommy text-[16px] font-bold leading-normal">
-                    Your Total Spins
-                  </span>
-                  <span className="text-[#FCE7C5] text-center font-made-tommy text-[16px] font-bold leading-[20px] bg-[#8F6E75] w-5 h-5 rounded-[5px]">
-                    7
+                  <div className="flex justify-center items-center gap-1">
+                    <span className="text-[#8F6E75] text-center font-made-tommy text-[16px] font-bold leading-normal">
+                      Your Total Spins
+                    </span>
+                    <span className="text-[#FCE7C5] text-center font-made-tommy text-[16px] font-bold leading-[20px] bg-[#8F6E75] w-5 h-5 rounded-[5px]">
+                      7
+                    </span>
+                  </div>
+                  <span className="text-[#745061] text-center font-made-tommy text-[16px] font-bold leading-normal">
+                    Want more Spins?
                   </span>
                 </div>
-                <span className="text-[#745061] text-center font-made-tommy text-[16px] font-bold leading-normal">
-                  Want more Spins?
-                </span>
-                  </div>
                 <div className="flex flex-col justify-center items-center gap-1 rounded-[14px] bg-[#E3BEAA]">
                   <div className="w-full py-1.5 border-b-1 border-b-[#EBD8BE]">
                     <span className="text-[#745061] text-center font-made-tommy text-[14px] font-bold leading-normal px-3">
@@ -84,11 +84,11 @@ const MoreSpinsDialog = ({ isOpen, onClose }: MoreSpinsDialogProps) => {
                   </div>
                   <div className="w-full rounded-b-[14px] p-[10px] bg-[#7450611A] flex gap-x-1">
                     <Button className='rounded-[4px] bg-gradient-to-b from-[#24BE62] from-[10%] to-[#1AB257] to-[201.67%] py-[3px] w-full flex gap-x-1 items-center justify-center'>
-                      <TelegramInviteIcon />
+                      <TelegramInviteIcon className="w-4 h-4" />
                       <span className='text-white text-sm font-bold py-[1px]'>Telegram</span>
                     </Button>
                     <Button className='rounded-[4px] bg-gradient-to-b from-[#24BE62] from-[10%] to-[#1AB257] to-[201.67%] py-[3px] w-full flex gap-x-1 items-center justify-center'>
-                      <InviteCopyIcon />
+                      <InviteCopyIcon className="w-4 h-4" />
                       <span className='text-white text-sm font-bold py-[1px]'>Copy Invite</span>
                     </Button>
                   </div>
