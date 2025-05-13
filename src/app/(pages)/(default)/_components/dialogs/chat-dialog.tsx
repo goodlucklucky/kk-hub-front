ddb2c65f15f3d877ef29362721b97979625948c0
Jeff Lingad <jeff@kokomo.games>
2025-05-13 08:40:39 -0500

    Implemented feedback on chat modal

--- src/app/(pages)/(default)/_components/dialogs/chat-dialog.tsx
+++ src/app/(pages)/(default)/_components/dialogs/chat-dialog.tsx
@@ -12,6 +12,7 @@ import startchat from "@assets/images/star-icon.png";
 import { SendBtnIcon } from "@/app/_assets/svg/sendbtn";
 import { DotIcon } from "@/app/_assets/svg/chat";
 import { CloseIcon } from "@/app/_assets/svg/close";
+import { ChatItem } from "@/app/(pages)/(default)/_components/dialogs/chat/chat-item";
 
 //interface
 interface ChatDialogProps {
@@ -40,159 +41,63 @@ const ChatDialog = ({ isOpen, onClose, }: ChatDialogProps) => {
 
           <div className="flex flex-col gap-1 px-[9px] py-2 bg-[#F5D6B1] rounded-2xl shadow-md border-2 border-[#A96415] flex-1 overflow-auto">
             <div className="rounded-[7px] border-2 border-[#CDAA98] bg-[#E3BEAA] shadow-[0px_4px_0px_0px_rgba(0,0,0,0.20)_inset] p-1 pt-2 flex flex-col flex-1 gap-y-1 overflow-auto">
-              <div className="w-full flex justify-between items-center bg-[#EED1B8] rounded-[7px] py-2 px-2.5 gap-x-2">
-                <div className="w-11 h-11 flex justify-center items-center rounded-full bg-[#D1B69F] shadow-[0px_2px_0px_0px_rgba(0,0,0,0.10)]">
-                  <DotIcon />
-                </div>
-                <div className="rounded-[10px] border border-[#C5B3E6] bg-[#F0E6FF] shadow-[0px_1px_0px_0px_rgba(0,0,0,0.10)] flex-1 p-1 relative pt-3.5 mt-3">
-                  <div className="text-[#653F56] text-[14px]/[14px] font-medium font-made-tommy">
-                    writes something smart that takes two lines
-                  </div>
-                  <div className="absolute -left-0 -top-0.5 bg-[#653F56] rounded-r-[5px] text-[#F0E6FF] p-[1px] px-1.5 flex gap-y-1">
-                    <div className="font-bumper-sticker text-[11px]/[12px] font-normal">
-                      LECOCONUT:
-                    </div>
-                    <div className="font-made-tommy text-[12px]/[12px] font-medium pl-1">
-                      writes something...
-                    </div>
-                  </div>
-                  <div className="flex justify-center items-center gap-x-1 absolute -top-[15px] left-0 bg-[#97757AA8] font-made-tommy text-[10px]/[10px] font-medium rounded-t-[4px] px-2 py-[1px] pt-[2px] text-[#EED1B8] [text-shadow:0px_1px_0px_rgba(0,0,0,0.20)]">
-                    Today, 14:47
-                  </div>
-                  <div className="flex justify-center items-center gap-x-1 absolute -top-[10px] right-0 rounded-[3px] border border-[#AB7EEC] bg-[#BA96FF] font-bumper-sticker text-[12px]/[12px] font-medium px-1.5 py-[1px] pt-[2px] text-[#EED1B8] [text-shadow:0px_1px_0px_rgba(0,0,0,0.20)]">
-                    LEBRUN134
-                    <Image src={startchat} alt="startchat" className="w-4 h-4 absolute -right-[10px]" />
-                  </div>
-                </div>
-              </div>
-              <div className="w-full flex justify-between items-center bg-[#EED1B8] rounded-[7px] py-2 px-2.5 gap-x-2">
-                <div className="rounded-[10px] border border-[#804306] bg-[#F9CB88] shadow-[0px_1px_0px_0px_rgba(0,0,0,0.10)] flex-1 p-1 relative pt-3.5 mt-3">
-                  <div className="text-[#804306] text-[14px]/[15px] font-medium font-made-tommy">
-                    and here comes just a very long message that eventually takes up three lines
-                  </div>
-                  <div className="absolute -right-0 -top-1 bg-[#653F56] rounded-l-[5px] text-[#F0E6FF] p-[1px] px-1.5 flex gap-y-1">
-                    <div className="font-bumper-sticker text-[11px]/[12px] font-normal">
-                      LECOCONUT:
-                    </div>
-                    <div className="font-made-tommy text-[12px]/[12px] font-medium pl-1">
-                      writes something...
-                    </div>
-                  </div>
-                  <div className="flex justify-center items-center gap-x-1 absolute -top-[16px] right-0 bg-[#97757AA8] font-made-tommy text-[10px]/[10px] font-medium rounded-t-[4px] px-2 py-[1px] pt-[2px] text-[#EED1B8] [text-shadow:0px_1px_0px_rgba(0,0,0,0.20)]">
-                    Today, 14:47
-                  </div>
-                  <div className="flex justify-center items-center gap-x-1 absolute -top-[10px] left-0 rounded-[3px] border border-[#AB7EEC] bg-[#BA96FF] font-bumper-sticker text-[12px]/[12px] font-medium px-1.5 py-[1px] pt-[2px] text-[#EED1B8] [text-shadow:0px_1px_0px_rgba(0,0,0,0.20)]">
-                    LEBRUN134
-                    <Image src={startchat} alt="startchat" className="w-4 h-4 absolute -left-[10px]" />
-                  </div>
-                </div>
-                <div className="w-11 h-11 flex justify-center items-center rounded-full bg-[#D1B69F] shadow-[0px_2px_0px_0px_rgba(0,0,0,0.10)]">
-                  <DotIcon />
-                </div>
-              </div>
-              <div className="w-full flex justify-between items-center bg-[#EED1B8] rounded-[7px] py-2 px-2.5 gap-x-2">
-                <div className="rounded-[10px] border border-[#804306] bg-[#F9CB88] shadow-[0px_1px_0px_0px_rgba(0,0,0,0.10)] flex-1 p-1 relative pt-3.5 mt-3">
-                  <div className="text-[#804306] text-[14px]/[15px] font-medium font-made-tommy">
-                    and here comes just a very long message that eventually takes up three lines
-                  </div>
-                  <div className="absolute -right-0 -top-1 bg-[#653F56] rounded-l-[5px] text-[#F0E6FF] p-[1px] px-1.5 flex gap-y-1">
-                    <div className="font-bumper-sticker text-[11px]/[12px] font-normal">
-                      LECOCONUT:
-                    </div>
-                    <div className="font-made-tommy text-[12px]/[12px] font-medium pl-1">
-                      writes something...
-                    </div>
-                  </div>
-                  <div className="flex justify-center items-center gap-x-1 absolute -top-[16px] right-0 bg-[#97757AA8] font-made-tommy text-[10px]/[10px] font-medium rounded-t-[4px] px-2 py-[1px] pt-[2px] text-[#EED1B8] [text-shadow:0px_1px_0px_rgba(0,0,0,0.20)]">
-                    Today, 14:47
-                  </div>
-                  <div className="flex justify-center items-center gap-x-1 absolute -top-[10px] left-0 rounded-[3px] border border-[#AB7EEC] bg-[#BA96FF] font-bumper-sticker text-[12px]/[12px] font-medium px-1.5 py-[1px] pt-[2px] text-[#EED1B8] [text-shadow:0px_1px_0px_rgba(0,0,0,0.20)]">
-                    LEBRUN134
-                    <Image src={startchat} alt="startchat" className="w-4 h-4 absolute -left-[10px]" />
-                  </div>
-                </div>
-                <div className="w-11 h-11 flex justify-center items-center rounded-full bg-[#653F56] shadow-[0px_2px_0px_0px_rgba(0,0,0,0.10)]">
-                  <DotIcon color="#E3BEAA" />
-                </div>
-              </div>
-              <div className="w-full flex justify-between items-center bg-[#EED1B8] rounded-[7px] py-2 px-2.5 gap-x-2">
-                <div className="rounded-[10px] border border-[#804306] bg-[#F9CB88] shadow-[0px_1px_0px_0px_rgba(0,0,0,0.10)] flex-1 p-1 relative pt-3.5 mt-3">
-                  <div className="text-[#804306] text-[14px]/[15px] font-medium font-made-tommy">
-                    and here comes just a very long message that eventually takes up three lines
-                  </div>
-                  <div className="absolute -right-0 -top-1 bg-[#653F56] rounded-l-[5px] text-[#F0E6FF] p-[1px] px-1.5 flex gap-y-1">
-                    <div className="font-bumper-sticker text-[11px]/[12px] font-normal">
-                      LECOCONUT:
-                    </div>
-                    <div className="font-made-tommy text-[12px]/[12px] font-medium pl-1">
-                      writes something...
-                    </div>
-                  </div>
-                  <div className="flex justify-center items-center gap-x-1 absolute -top-[16px] right-0 bg-[#97757AA8] font-made-tommy text-[10px]/[10px] font-medium rounded-t-[4px] px-2 py-[1px] pt-[2px] text-[#EED1B8] [text-shadow:0px_1px_0px_rgba(0,0,0,0.20)]">
-                    Today, 14:47
-                  </div>
-                  <div className="flex justify-center items-center gap-x-1 absolute -top-[10px] left-0 rounded-[3px] border border-[#AB7EEC] bg-[#BA96FF] font-bumper-sticker text-[12px]/[12px] font-medium px-1.5 py-[1px] pt-[2px] text-[#EED1B8] [text-shadow:0px_1px_0px_rgba(0,0,0,0.20)]">
-                    LEBRUN134
-                    <Image src={startchat} alt="startchat" className="w-4 h-4 absolute -left-[10px]" />
-                  </div>
-                </div>
-                <div className="w-11 h-11 flex justify-center items-center rounded-full bg-[#D1B69F] shadow-[0px_2px_0px_0px_rgba(0,0,0,0.10)]">
-                  <DotIcon />
-                </div>
-              </div>
-              <div className="w-full flex justify-between items-center bg-[#EED1B8] rounded-[7px] py-2 px-2.5 gap-x-2">
-                <div className="w-11 h-11 flex justify-center items-center rounded-full bg-[#D1B69F] shadow-[0px_2px_0px_0px_rgba(0,0,0,0.10)]">
-                  <DotIcon />
-                </div>
-                <div className="rounded-[10px] border border-[#C5B3E6] bg-[#F0E6FF] shadow-[0px_1px_0px_0px_rgba(0,0,0,0.10)] flex-1 p-1 relative pt-3.5 mt-3">
-                  <div className="text-[#653F56] text-[14px]/[14px] font-medium font-made-tommy">
-                    writes something smart that takes two lines
-                  </div>
-                  <div className="absolute -left-0 -top-0.5 bg-[#653F56] rounded-r-[5px] text-[#F0E6FF] p-[1px] px-1.5 flex gap-y-1">
-                    <div className="font-bumper-sticker text-[11px]/[12px] font-normal">
-                      LECOCONUT:
-                    </div>
-                    <div className="font-made-tommy text-[12px]/[12px] font-medium pl-1">
-                      writes something...
-                    </div>
-                  </div>
-                  <div className="flex justify-center items-center gap-x-1 absolute -top-[15px] left-0 bg-[#97757AA8] font-made-tommy text-[10px]/[10px] font-medium rounded-t-[4px] px-2 py-[1px] pt-[2px] text-[#EED1B8] [text-shadow:0px_1px_0px_rgba(0,0,0,0.20)]">
-                    Today, 14:47
-                  </div>
-                  <div className="flex justify-center items-center gap-x-1 absolute -top-[10px] right-0 rounded-[3px] border border-[#AB7EEC] bg-[#BA96FF] font-bumper-sticker text-[12px]/[12px] font-medium px-1.5 py-[1px] pt-[2px] text-[#EED1B8] [text-shadow:0px_1px_0px_rgba(0,0,0,0.20)]">
-                    LEBRUN134
-                    <Image src={startchat} alt="startchat" className="w-4 h-4 absolute -right-[10px]" />
-                  </div>
-                </div>
-              </div>
-              <div className="w-full flex justify-between items-center bg-[#EED1B8] rounded-[7px] py-2 px-2.5 gap-x-2">
-                <div className="w-11 h-11 flex justify-center items-center rounded-full bg-[#D1B69F] shadow-[0px_2px_0px_0px_rgba(0,0,0,0.10)]">
-                  <DotIcon />
-                </div>
-                <div className="rounded-[10px] border border-[#C5B3E6] bg-[#F0E6FF] shadow-[0px_1px_0px_0px_rgba(0,0,0,0.10)] flex-1 p-1 relative pt-3.5 mt-3">
-                  <div className="text-[#653F56] text-[14px]/[14px] font-medium font-made-tommy">
-                    writes something smart that takes two lines
-                  </div>
-                  <div className="absolute -left-0 -top-0.5 bg-[#653F56] rounded-r-[5px] text-[#F0E6FF] p-[1px] px-1.5 flex gap-y-1">
-                    <div className="font-bumper-sticker text-[11px]/[12px] font-normal">
-                      LECOCONUT:
-                    </div>
-                    <div className="font-made-tommy text-[12px]/[12px] font-medium pl-1">
-                      writes something...
-                    </div>
-                  </div>
-                  <div className="flex justify-center items-center gap-x-1 absolute -top-[15px] left-0 bg-[#97757AA8] font-made-tommy text-[10px]/[10px] font-medium rounded-t-[4px] px-2 py-[1px] pt-[2px] text-[#EED1B8] [text-shadow:0px_1px_0px_rgba(0,0,0,0.20)]">
-                    Today, 14:47
-                  </div>
-                  <div className="flex justify-center items-center gap-x-1 absolute -top-[10px] right-0 rounded-[3px] border border-[#AB7EEC] bg-[#BA96FF] font-bumper-sticker text-[12px]/[12px] font-medium px-1.5 py-[1px] pt-[2px] text-[#EED1B8] [text-shadow:0px_1px_0px_rgba(0,0,0,0.20)]">
-                    LEBRUN134
-                    <Image src={startchat} alt="startchat" className="w-4 h-4 absolute -right-[10px]" />
-                  </div>
-                </div>
-              </div>
+              <ChatItem
+                sender="LECOCONUT"
+                message="writes something smart that takes two lines"
+                time="Today, 14:47"
+                isSender={true}
+                replyTo="LEBRUN134"
+                replyMessage="writes something..."
+              />
+              <ChatItem
+                sender="LEPINEAPPLE"
+                message="and here comes just a very long message that eventually takes up three lines"
+                time="Today, 14:47"
+                isSender={false}
+                replyTo="LECOCONUT"
+                replyMessage="writes something..."
+              />
+              <ChatItem
+                sender="LEPINEAPPLE"
+                message="lol"
+                time="Today, 14:47"
+                isSender={false}
+              />
+              <ChatItem
+                sender="LEPINEAPPLE"
+                message="and here comes just a very long message that eventually takes up three lines"
+                time="Today, 14:47"
+                isSender={false}
+                replyTo="LECOCONUT"
+                replyMessage="writes something..."
+              />
+              <ChatItem
+                sender="LECOCONUT"
+                message="writes something smart that takes two lines"
+                time="Today, 14:47"
+                isSender={true}
+                replyTo="LEBRUN134"
+                replyMessage="writes something..."
+              />
+              <ChatItem
+                sender="LECOCONUT"
+                message="writes something smart that takes two lines"
+                time="Today, 14:47"
+                isSender={true}
+                replyTo="LEBRUN134"
+                replyMessage="writes something..."
+              />
             </div>
             <div className="flex gap-x-1">
-              <textarea className="w-full rounded-[5px] border-2 border-[#D2AA87] bg-[##F4F4F0] shadow-[0px_1px_0px_0px_rgba(0,0,0,0.20)] text-[#653F56] text-[16px] font-made-tommy font-semibold px-3 py-2 h-11" rows={1}></textarea>
+              <textarea 
+                className="w-full rounded-[5px] border-2 border-[#D2AA87] bg-[#F4F4F0] shadow-[0px_1px_0px_0px_rgba(0,0,0,0.20)] text-[#653F56] text-[16px] outline-none font-made-tommy font-semibold px-3 py-2 min-h-[44px] max-h-[96px] resize-none overflow-y-auto"
+                rows={1}
+                onInput={(e) => {
+                  const target = e.target as HTMLTextAreaElement;
+                  target.style.height = 'auto';
+                  target.style.height = `${target.scrollHeight}px`;
+                }}
+              ></textarea>
               <SendBtnIcon />
             </div>
           </div>
