3215a81c7fca7aa5a637c18ab3e5606b02add78f
Jeff Lingad <jeff@kokomo.games>
2025-06-03 15:46:55 -0500

    fix: first landing page accessibility without login

--- src/app/(pages)/(default)/layout.tsx
+++ src/app/(pages)/(default)/layout.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import Header from "./_components/layout/header";
 import { cn } from "@/app/_lib/utils";
 import Footer from "./_components/layout/footer";
+import { ThirdWeb } from "@/app/_providers/thirdWebProvider";
 
 export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className={cn("min-h-dvh flex flex-col max-h-dvh")}>
-      <Header />
-      <main className="flex flex-col h-[calc(100vh-172px)] z-0">
-        {children}
-      </main>
-      <Footer footerCategory="home" />
-    </div>
+    <ThirdWeb>
+      <div className={cn("min-h-dvh flex flex-col max-h-dvh")}>
+        <Header />
+        <main className="flex flex-col h-[calc(100vh-172px)] z-0">
+          {children}
+        </main>
+        <Footer footerCategory="home" />
+      </div>
+    </ThirdWeb>
   );
 }
